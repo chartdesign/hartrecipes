3fa9056931feed3fbc53f2c43b7f1b306d711bb0
hartdesign <mayjahleaguebeats@gmail.com>
2017-04-16 06:00:12 +0000

    added edit and delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,6 @@ class RecipesController < ApplicationController
   end
   
   def create
-  
     @recipe = Recipe.new(recipe_params)
     @recipe.chef = Chef.first
     if @recipe.save
@@ -22,7 +21,26 @@ class RecipesController < ApplicationController
     else
       render 'new'
     end
-    
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully..."
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe was deleted successfully..."
+    redirect_to recipes_path
   end
   
   private 
