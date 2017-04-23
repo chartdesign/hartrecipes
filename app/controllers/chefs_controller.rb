a93aceface24cdbdf90482e892cbc560e3cfab18
hartdesign <mayjahleaguebeats@gmail.com>
2017-04-23 06:31:46 +0000

    add chef edit action with test

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -3,6 +3,10 @@ class ChefsController < ApplicationController
     @chef = Chef.new
   end
   
+  def index
+    @chef = Chef.all
+  end
+  
   def create
     @chef = Chef.new(chef_params)
     if @chef.save
@@ -15,6 +19,20 @@ class ChefsController < ApplicationController
   
   def show
     @chef = Chef.find(params[:id])
+  end
+  
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+  
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to @chef
+    else
+      render 'edit'
+    end  
   end
   
   private
